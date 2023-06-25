b8652c37fd05dfe59e82f85d8a39690243e26117
melvin145 <pythonnoobie4@gmail.com>
2023-06-25 20:12:21 +0530

    final

--- newsletter-sign-up-with-success-message-main/script.js
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -1,11 +1,31 @@
 const mainContainer=document.getElementById("mainContainer");
 const popupContainer=document.getElementById("popupContainer");
 const container=document.getElementById("container");
+const email=document.getElementById("emailId");
+const errmsg=document.getElementById("errmsg");
+const submitbtn=document.getElementById('subscribebtn');
 
-function OpenPopup(){
-      container.classList.add(".showpopup");
-      console.log("open");
+submitbtn.addEventListener('click',Checker);
+function OpenPopup(email){
+      const popupemail=document.getElementById('popupemail');
+      popupemail.innerText=email;
+      popupContainer.classList.add("showpopup");
 };
 function ClosePopup(){
-      
+      popupContainer.classList.remove('showpopup');
+}
+
+function Checker(){
+      let validreg=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      if(email.value.match(validreg)){
+            errmsg.style.display='none';
+            OpenPopup(email.value);
+            email.value='';
+      }
+      else if(email.value=""){
+            errmsg.style.display="none";
+      }
+      else{
+            errmsg.style.display='block';
+      }
 }
