16ec6d17f5cd1c3db477daf1cc0e93b03ea5c2cc
melvin145 <pythonnoobie4@gmail.com>
2023-06-25 18:53:03 +0530

    s

---
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -0,0 +1,11 @@
+const mainContainer=document.getElementById("mainContainer");
+const popupContainer=document.getElementById("popupContainer");
+const container=document.getElementById("container");
+
+function OpenPopup(){
+      container.classList.add(".showpopup");
+      console.log("open");
+};
+function ClosePopup(){
+      
+}
