4facc0097ba0c156377212a8bad463ff85cd48b1
melvin145 <pythonnoobie4@gmail.com>
2023-07-06 20:13:10 +0530

    d

--- intro-section-with-dropdown-navigation-main/script.js
+++ intro-section-with-dropdown-navigation-main/script.js
@@ -5,7 +5,7 @@ function ToggleHandler(){
       menu.addEventListener('click',()=>{
             nav.classList.toggle('show_menu');
             menu.classList.add('hidden')
-            close.classList.remove('hidden');
+            close.classList.remove('');
 
       })
       close.addEventListener('click',()=>{
